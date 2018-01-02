0ad60e9324d146e9bfcdadbca02411e80fee6105
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2018-01-02 15:56:55 +0530

    bootstrap and basic features added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import React,{ Component } from 'react';
-
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component{
+constructor(props){
+super(props);
+this.state={
+query:''
+}
+}
+
+search(){
+const BASE_URL='https://api.spotify.com/v1/search'
+}
+
 render(){
 return(
-<div>
-
+<div className="App">
 <div className="App-title">Music Master</div>
-<div>
-<input placeholder="search an artist..." />
-<button>button</button>
+<FormGroup>
+  <InputGroup>
+    <FormControl
+      type="text"
+      placeholder="Search Artist"
+      value={this.state.query}
+      onChange={event => {this.setState({query: event.target.value})}}
+    />
+<InputGroup.Addon onClick={() => this.search()}>
+   <Glyphicon glyph="search"></Glyphicon>
+</InputGroup.Addon>
+  </InputGroup>
 
-</div>
+</FormGroup>
 <div className="Profile">
   <div>Artist Picture</div>
   <div>Artist Name</div>
